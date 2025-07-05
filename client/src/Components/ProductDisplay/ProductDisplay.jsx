7dc1150f11745354f3aa751c27d3e481a177ebee
Loveth <lovazodo@gmail.com>
2025-07-05 20:04:51 +0100

    fixing product display image

--- client/src/Components/ProductDisplay/ProductDisplay.jsx
+++ client/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,80 +1,84 @@
 import React, { useContext } from 'react';
 import './ProductDisplay.css';
-import star_icon from '../Assets/star_icon.png'
+import star_icon from '../Assets/star_icon.png';
 import star_dull_icon from '../Assets/star_dull_icon.png';
 import { ShopContext } from '../../Context/ShopContent';
 import toast, { Toaster } from 'react-hot-toast';
 
+const ProductDisplay = ({ product }) => {
+  const { addToCart, addToWishlist } = useContext(ShopContext);
+  const backendUrl = 'https://zanga-dtb7.onrender.com';
 
+  const imageUrl = `${backendUrl}/images/${product.image}`;
 
+  const notification = () => toast.success(`${product.name} has been added to cart`);
+  const wishlistNotification = () => toast.success(`${product.name} has been added to wishlist`);
 
-const ProductDisplay = (props) => {
-    const {product} = props
-    const {addToCart, addToWishlist} = useContext(ShopContext);
-
-
-    const notification = () => toast.success(`${product.name} has been added to cart`);
-    const wishlistnotification = () => toast.success(`${product.name} has been added to wishlist`);
   return (
     <div className='productDisplay'>
-    <div className="productDisplay-left">
-<div className="productDisplay-img-list">
-    <img src={product.image} alt="" />
-    <img src={product.image} alt="" />
-    <img src={product.image} alt="" />
-    <img src={product.image} alt="" />
-</div>
+      <div className="productDisplay-left">
+        <div className="productDisplay-img-list">
+          <img src={imageUrl} alt={product.name} />
+          <img src={imageUrl} alt={product.name} />
+          <img src={imageUrl} alt={product.name} />
+          <img src={imageUrl} alt={product.name} />
+        </div>
 
-<div className="productDisplay-img">
-    <img className='productDisplay-main-img' src={product.image} alt="" />
-</div> 
-    </div>
+        <div className="productDisplay-img">
+          <img className='productDisplay-main-img' src={imageUrl} alt={product.name} />
+        </div>
+      </div>
 
-    <div className="productDisplay-right">
+      <div className="productDisplay-right">
         <h1>{product.name}</h1>
+
         <div className="productDisplay-right-star">
-            <img src={star_icon} alt="" />
-            <img src={star_icon} alt="" />
-            <img src={star_icon} alt="" />
-            <img src={star_icon} alt="" />
-            <img src={star_dull_icon} alt="" />
-            <p>(122)</p>
+          <img src={star_icon} alt="star" />
+          <img src={star_icon} alt="star" />
+          <img src={star_icon} alt="star" />
+          <img src={star_icon} alt="star" />
+          <img src={star_dull_icon} alt="star dull" />
+          <p>(122)</p>
         </div>
 
         <div className="productDisplay-right-prices">
-            <div className="productDisplay-right-price-old">${product.old_price}</div>
-        <div className="productDisplay-right-price-new">${product.new_price}</div>
+          <div className="productDisplay-right-price-old">₦{product.old_price}</div>
+          <div className="productDisplay-right-price-new">₦{product.new_price}</div>
         </div>
 
         <div className="productDisplay-right-description">
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-Nam in nibh et arcu tempus convallis in sit amet leo. 
-Sed eu pulvinar mauris. Ut ut tempus purus, eget laoreet neque. 
-Aenean porttitor, metus eu tristique dignissim, erat elit finibus felis, 
-in lacinia magna tellus at velit. Fusce mollis tellus eget urna faucibus, 
-vel sagittis lacus placerat. Quisque non volutpat nulla, ac ullamcorper arcu.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in nibh et arcu
+            tempus convallis in sit amet leo...
+          </p>
         </div>
+
         <div className="productDisplay-right-size">
-            <h1>Select Size</h1>
-            <div className="productDisplay-right-sizes">
-                <div>S</div>
-                <div>M</div>
-                <div>L</div>
-                <div>XL</div>
-                <div>XXL</div>
-            </div>
+          <h1>Select Size</h1>
+          <div className="productDisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
         </div>
+
         <div className="cart-wishlist-btn">
-        <button onClick={() =>{addToCart(product.id); notification()}}>ADD TO CART</button>
-        <Toaster />
-        <button onClick={() =>{addToWishlist(product.id); wishlistnotification()}}>ADD TO WISHLIST</button>
+          <button onClick={() => { addToCart(product.id); notification(); }}>ADD TO CART</button>
+          <Toaster />
+          <button onClick={() => { addToWishlist(product.id); wishlistNotification(); }}>ADD TO WISHLIST</button>
         </div>
-        <p className='productDisplay-right-category'> <span>Category :</span> {product.category}</p>
-        <p className='productDisplay-right-category'> <span>Tags :</span> Modern, Latest</p>
-    </div>
 
+        <p className='productDisplay-right-category'>
+          <span>Category :</span> {product.category}
+        </p>
+        <p className='productDisplay-right-category'>
+          <span>Tags :</span> Modern, Latest
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDisplay
+export default ProductDisplay;
