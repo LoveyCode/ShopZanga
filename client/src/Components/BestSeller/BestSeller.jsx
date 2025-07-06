a64e55cdd1e34143d44468c212d282f089b26cb5
Loveth <lovazodo@gmail.com>
2025-07-06 23:08:50 +0100

    cloudinary fix

--- client/src/Components/BestSeller/BestSeller.jsx
+++ client/src/Components/BestSeller/BestSeller.jsx
@@ -12,43 +12,33 @@ function BestSeller(props) {
   const wishlistnotification = () => toast.success(`${props.name} has been added to wishlist`);
 
 
-  const backendUrl = 'https://zanga-dtb7.onrender.com';
-  const imageUrl = `${backendUrl}/images/${props.image}`;
+const imageUrl = props.image;
 
-  return (
- 
-    <div className="item"> 
-   
-<Link to={`/product/${props.id}`}><img   src={imageUrl} alt="" /> </Link>
-<Toaster />
-<div className="item-description">
-<Link to={`/product/${props.id}`}>
-<img className='item-description-big-img'  
-   src={imageUrl} alt={props.name} /> 
-</Link>
-
-<div className="item-description-images">
-
- <img onClick={() =>{addToCart(props.id); notification()}} src={cart} className='cart' alt="cart" /> 
- <img onClick={() =>{ addToWishlist(props.id); wishlistnotification()}} src={heart} className='heart-icon' alt="wish-list" /> 
- </div>
- </div>
-    
- <p className='item-link'><Link to={`/product/${props.id}`}>{props.name}</Link></p>
-<div className="item-prices">
-  <div className="item-price-new">
-${props.new_price}
+return (
+  <div className="item"> 
+    <Link to={`/product/${props.id}`}>
+      <img src={imageUrl} alt={props.name} />
+    </Link>
+    <Toaster />
+    <div className="item-description">
+      <Link to={`/product/${props.id}`}>
+        <img className='item-description-big-img' src={imageUrl} alt={props.name} />
+      </Link>
+      <div className="item-description-images">
+        <img onClick={() => { addToCart(props.id); notification(); }} src={cart} className='cart' alt="cart" /> 
+        <img onClick={() => { addToWishlist(props.id); wishlistnotification(); }} src={heart} className='heart-icon' alt="wish-list" /> 
+      </div>
+    </div>
+    <p className='item-link'>
+      <Link to={`/product/${props.id}`}>{props.name}</Link>
+    </p>
+    <div className="item-prices">
+      <div className="item-price-new">${props.new_price}</div>
+      <div className="item-price-old">${props.old_price}</div>
+    </div>
   </div>
-  <div className="item-price-old">
-  ${props.old_price}
-    </div>
-</div>
+);
 
-
-    </div>
-
-    
-  )
 }
 
 export default BestSeller
