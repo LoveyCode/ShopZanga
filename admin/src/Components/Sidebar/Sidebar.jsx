5fc23aba5eae782bfb0fb8b772ceacc0ee42ca68
Loveth <lovazodo@gmail.com>
2025-07-09 17:56:07 +0100

    restructuring of the admin

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -7,14 +7,14 @@ import list_product_icon from '../../assets/list_product_icon.png'
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-<Link to={'/addproduct'} style={{textDecoration: 'none'}}>
+<Link to={'/admin/addproduct'} style={{textDecoration: 'none'}}>
 <div className="sidebar-item">
     <img src={add_product_icon} alt="" />
     <p>Add Product</p>
 </div>
 </Link>
 
-<Link to={'/listproduct'} style={{textDecoration: 'none'}}>
+<Link to={'/admin/listproduct'} style={{textDecoration: 'none'}}>
 <div className="sidebar-item">
     <img src={list_product_icon} alt="" />
     <p>Product List</p>
--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,20 +1,17 @@
-import React from 'react'
-import './Admin.css'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-import { Routes, Route } from 'react-router-dom'
-import AddProduct from '../../Components/AddProduct/AddProduct'
-import ListProduct from '../../Components/ListProduct/ListProduct'
+import React from 'react';
+import './Admin.css';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import { Outlet } from 'react-router-dom';
 
 const Admin = () => {
   return (
     <div className='admin'>
-<Sidebar />
-<Routes>
-  <Route path='/addproduct' element ={<AddProduct />} />
-  <Route path='/listproduct' element ={<ListProduct />} />
-</Routes>
+      <Sidebar />
+      <div className="admin-content">
+        <Outlet /> {/* This renders AddProduct or ListProduct */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
