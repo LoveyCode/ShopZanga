ef07cd59a26a0a356d55cf9a626f98f3314fec7a
Loveth <lovazodo@gmail.com>
2025-07-09 19:09:37 +0100

    adding render routes

--- admin/src/Pages/adminLogin/adminLogin.jsx
+++ admin/src/Pages/adminLogin/adminLogin.jsx
@@ -10,7 +10,7 @@ const AdminLogin = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:4000/admin/login', {
+      const response = await fetch('https://zanga-dtb7.onrender.com/admin/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
